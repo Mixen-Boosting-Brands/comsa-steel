ecd0d00e8ad1bba726952dd51b1f7ad757781238
Luis Carlos Pando <hey@luiscarlospando.com>
2024-05-09 14:18:29 -0600

    Agregar transparencia a navbar al hacer scroll

--- footer.php
+++ footer.php
@@ -113,6 +113,6 @@
 
 	<script defer src="<?php echo esc_url(
      get_template_directory_uri()
- ); ?>/assets/js/app.964a52be2a7024977d36.bundle.js"></script>
+ ); ?>/assets/js/app.4650445e352b7d4bd5b0.bundle.js"></script>
 </body>
 </html>
