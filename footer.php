531b83e86fd79eb53ca1cf09cb8110ff3f41bbca
Luis Carlos Pando <hey@luiscarlospando.com>
2024-05-09 14:38:42 -0600

    Ajustes JP

--- footer.php
+++ footer.php
@@ -113,6 +113,6 @@
 
 	<script defer src="<?php echo esc_url(
      get_template_directory_uri()
- ); ?>/assets/js/app.4650445e352b7d4bd5b0.bundle.js"></script>
+ ); ?>/assets/js/app.3d55c4965cfcc0b86221.bundle.js"></script>
 </body>
 </html>
