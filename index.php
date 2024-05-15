084c2905d50c58252bcdf16d9570e37830fe978c
Luis Carlos Pando <hey@luiscarlospando.com>
2024-05-15 17:46:57 -0600

    Agregar video en español

--- index.php
+++ index.php
@@ -123,14 +123,14 @@ get_header();
         <div class="container">
             <div class="row">
                 <div class="col">
-                    <?php if (!($currentlang == "en-US")): ?>
+                    <?php if ($currentlang == "en-US"): ?>
                     <div style="padding:56.25% 0 0 0;position:relative;" data-aos="fade-up" data-aos-duration="1000">
                         <iframe src="https://player.vimeo.com/video/946310996?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="COMSA Steel"></iframe>
                     </div>
                     <script src="https://player.vimeo.com/api/player.js"></script>
                     <?php else: ?>
-                    <div style="padding:56.25% 0 0 0;position:relative;" data-aos="fade-up" data-aos-duration="1000">
-                        <iframe src="https://player.vimeo.com/video/946310996?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="COMSA Steel"></iframe>
+                    <div style="padding:56.25% 0 0 0;position:relative;">
+                        <iframe src="https://player.vimeo.com/video/946799657?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="COMSA Steel"></iframe>
                     </div>
                     <script src="https://player.vimeo.com/api/player.js"></script>
                     <?php endif; ?>
