1d57773ba0096b6f530e1f3d955467e800dd8db6
Luis Carlos Pando <hey@luiscarlospando.com>
2024-05-15 17:34:48 -0600

    Agregar animación de entrada a video Vimeo

--- index.php
+++ index.php
@@ -123,7 +123,7 @@ get_header();
         <div class="container">
             <div class="row">
                 <div class="col">
-                    <div style="padding:56.25% 0 0 0;position:relative;">
+                    <div style="padding:56.25% 0 0 0;position:relative;" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="1250">
                         <iframe src="https://player.vimeo.com/video/946310996?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="COMSA Steel"></iframe>
                     </div>
                     <script src="https://player.vimeo.com/api/player.js"></script>
