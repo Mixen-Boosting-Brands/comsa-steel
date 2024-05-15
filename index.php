3c4f35ed70a653d1f1e87e755d8493bf240c2f87
Luis Carlos Pando <hey@luiscarlospando.com>
2024-05-15 17:33:11 -0600

    Poner video vimeo en un contenedor

--- index.php
+++ index.php
@@ -119,8 +119,13 @@ get_header();
         </div>
     </section>
 
-    <section id="video">
-        <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/946310996?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="COMSA Steel"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+    <section id="video" class="py-60">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/946310996?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="COMSA Steel"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+                </div>
+            </div>
     </section>
 
     <section id="grupo-san-francisco" class="common-light py-60">
@@ -725,7 +730,7 @@ wp_reset_postdata();
     <section id="contacto" class="common-light py-60">
         <div class="container">
             <div class="row mb-4">
-                <div class="col">
+                <div class="col text-center">
                     <h1><?php _e("Encuéntranos", "comsa-steel"); ?></h1>
                 </div>
             </div>
