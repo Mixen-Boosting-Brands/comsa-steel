81a186cae03308c046870512a981e1a9dc4fd72f
Luis Carlos Pando <hey@luiscarlospando.com>
2024-05-16 13:05:16 -0600

    Disminuir tamaño de logos de los clientes

--- footer.php
+++ footer.php
@@ -246,7 +246,7 @@
 
 	<script defer src="<?php echo esc_url(
      get_template_directory_uri()
- ); ?>/assets/js/app.c86527cffac544922996.bundle.js"></script>
+ ); ?>/assets/js/app.d7c2178333f22fe29593.bundle.js"></script>
 
 	<!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-H25C4HNG5N"></script>
