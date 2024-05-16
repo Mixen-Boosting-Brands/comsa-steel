f15eb6c8435b8fe1df93a8458b8b6e4585017f4d
Luis Carlos Pando <hey@luiscarlospando.com>
2024-05-16 14:19:35 -0600

    Agregar opacidad al overlay

--- footer.php
+++ footer.php
@@ -246,7 +246,7 @@
 
 	<script defer src="<?php echo esc_url(
      get_template_directory_uri()
- ); ?>/assets/js/app.254e058e958dc459e0cf.bundle.js"></script>
+ ); ?>/assets/js/app.b52b75cbb2f8eb42adf3.bundle.js"></script>
 
 	<!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-H25C4HNG5N"></script>
