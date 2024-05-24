7756e2e0dbd82b772de6f321d3b9923e08a73df6
Luis Carlos Pando <hey@luiscarlospando.com>
2024-05-24 15:56:03 -0600

    Quitar markup de swiper

--- page-contacto.php
+++ page-contacto.php
@@ -12,7 +12,7 @@ get_header(); ?>
             ); ?>/assets/videos/bg-video.mp4" type="video/mp4"">
             Your browser does not support the video tag.
         </video>
-        <div class="container" style="position: relative; z-index: 3;">
+        <div class="container" style="position: relative; height: auto; z-index: 3;">
             <div class="row" style="height: auto;">
                 <div class="col">
                     <hr>
