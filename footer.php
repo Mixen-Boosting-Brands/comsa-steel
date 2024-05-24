3ace099ac29e61b6b59046840e4c582699c54c56
Luis Carlos Pando <hey@luiscarlospando.com>
2024-05-24 09:52:57 -0600

    Quitar transición lineal del carrusel de clientes

--- footer.php
+++ footer.php
@@ -236,7 +236,7 @@
 
 	<script defer src="<?php echo esc_url(
      get_template_directory_uri()
- ); ?>/assets/js/app.86dfb5a1b6f3617c6379.bundle.js"></script>
+ ); ?>/assets/js/app.aba7c6c055e8c4e71c3d.bundle.js"></script>
 
 	<!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-H25C4HNG5N"></script>
