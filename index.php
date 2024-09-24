c8939e7c6cb6fcaf8dd455c41e22416b7ce10523
Luis Carlos Pando <hey@luiscarlospando.com>
2024-09-24 15:20:13 -0600

    Corregir condicional

--- index.php
+++ index.php
@@ -615,7 +615,7 @@ get_header();
 
     <?php if (!($currentlang == "en-US")): ?>
 <?php
-if (!($currentlang == "en-US")) {
+if ( $currentlang == "en-US" ) {
     $args = [
         "category_name" => "news", // Specify the category slug here
         "posts_per_page" => -1, // Use -1 to fetch all posts from the category
