fc041430666c9e7200f6676c39b641966ce5cf23
Luis Carlos Pando <hey@luiscarlospando.com>
2024-10-07 10:05:06 -0600

    Actualizar videos con las últimas versiones

--- index.php
+++ index.php
@@ -144,7 +144,7 @@ get_header();
                     <?php if ($currentlang == "en-US"): ?>
                     <div style="padding:75% 0 0 0;position:relative;" data-aos="fade-up" data-aos-duration="1000"><iframe src="https://player.vimeo.com/video/1012441669?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="COMSA Corporate Video"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
                     <?php else: ?>
-                    <div style="padding:75% 0 0 0;position:relative;" data-aos="fade-up" data-aos-duration="1000"><iframe src="https://player.vimeo.com/video/1012441669?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="COMSA Corporate Video"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+                    <div style="padding:56.25% 0 0 0;position:relative;" data-aos="fade-up" data-aos-duration="1000"><iframe src="https://player.vimeo.com/video/946799657?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="COMSA Steel"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
                     <?php endif; ?>
                 </div>
             </div>
