72629feea8b50654e51c035b94b9e2515bc7a51d
Luis Carlos Pando <hey@luiscarlospando.com>
2025-02-14 09:33:42 -0600

    Agregar código de conversiones de LinkedIn

--- footer.php
+++ footer.php
@@ -202,5 +202,8 @@ endif; ?>">
             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
         })(window, document, "clarity", "script", "prxlqi93kk");
     </script>
+
+    <!-- LinkedIn -->
+    <script type="text/javascript"> _linkedin_partner_id = "6699612"; window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || []; window._linkedin_data_partner_ids.push(_linkedin_partner_id); </script><script type="text/javascript"> (function(l) { if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])}; window.lintrk.q=[]} var s = document.getElementsByTagName("script")[0]; var b = document.createElement("script"); b.type = "text/javascript";b.async = true; b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js"; s.parentNode.insertBefore(b, s);})(window.lintrk); </script> <noscript> <img height="1" width="1" style="display:none;" alt="" src="https://px.ads.linkedin.com/collect/?pid=6699612&fmt=gif" /> </noscript>
 </body>
 </html>
